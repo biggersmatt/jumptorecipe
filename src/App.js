c3a0b36dd38b968f314a40357e739578ca00c056
Matt Biggers <matt.biggers@workday.com>
2022-08-26 16:46:18 -0700

    Sets up Routes connected to pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
+// Dependencies
+import {Route, Routes} from 'react-router-dom';
+
+// Pages
+
+// Components
 import Nav from './components/Nav/nav';
 import MyRecipes from './pages/myRecipes/myRecipes';
 import Recipe from './pages/recipe/recipe';
 import NewRecipe from './pages/newRecipe/newRecipe';
+
+// CSS
 import './App.css';
 
 function App() {
   return (
     <div>
       <Nav/>
-      <div>
-        <MyRecipes/>
-        <Recipe/>
-        <NewRecipe/>
-      </div>
+      <Routes>
+        <Route path='/myrecipes' element={<MyRecipes/>}/>
+        <Route path='/recipe' element={<Recipe/>}/>
+        <Route path='/new' element={<NewRecipe/>}/>
+      </Routes>
     </div>
   );
 }
