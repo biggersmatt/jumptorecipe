a09fbf92ed7048a1207409fa17cb6bac81d0b6bc
Matt Biggers <matt.biggers@workday.com>
2022-08-26 19:56:12 -0700

    Individual recipes in the list

--- src/components/MyRecipe/myRecipe.jsx
+++ src/components/MyRecipe/myRecipe.jsx
@@ -1,10 +1,23 @@
 // CSS
 import './myRecipe.css';
+import image from './tofu-scramble.jpeg';
 
 export default function MyRecipe(props) {
   return (
     <div className='myrecipe-wrapper'>
-      <h1>{props.recipe.name}</h1>
+      <div className='myrecipe-top'>
+        <h1>{props.recipe.name}</h1>
+        <h2>{props.recipe.time}</h2>
+      </div>
+      <div className="myrecipe-mid">
+        <img src={image} alt="tofu-scramble" />
+        <p>{props.recipe.description}</p>
+      </div>
+      <ul className='myrecipe-bottom'>
+        <li>Vegan</li>
+        <li>|</li>
+        <li>Breakfast</li>
+      </ul>
     </div>
   )
 }
