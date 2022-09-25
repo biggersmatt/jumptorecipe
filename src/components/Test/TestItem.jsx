22f6140c1218fdbe2ae874a68c05f408014ec6fb
Matt Biggers <matt.biggers@workday.com>
2022-09-24 17:55:48 -0700

    Testing for responsive design complete, may remove image

--- src/components/Test/TestItem.jsx
+++ src/components/Test/TestItem.jsx
@@ -1,7 +1,23 @@
+import tofu from './tofu-scramble.jpeg';
+
 export default function TestItem() {
   return (
     <div className="item">
-      <h3>Item</h3>
+      <div className='item-top'>
+        <h2>Tofu Scramble</h2>
+        <p>20mins</p>
+      </div>
+      <div className='item-middle'>
+        {/* <img src={tofu} alt="" /> */}
+        <p>A high protein alternative to scrambled eggs! With the right spice, this scramble tastes better than traditional egg scrambles.</p>
+      </div>
+      <div className='item-bottom'>
+        <div className='item-bottom-wrapper'>
+          <p>Vegan</p>
+          <p>|</p>
+          <p>Breakfast</p>
+        </div>
+      </div>
     </div>
   )
 }
