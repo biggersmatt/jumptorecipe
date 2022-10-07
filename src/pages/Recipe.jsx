6ba7cf747ca6cd21029fa04f5f962b92f5de743e
Matt Biggers <matt.biggers@workday.com>
2022-10-06 17:47:16 -0700

    Sets up initial styles for recipe page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -2,8 +2,25 @@ import '../components/Recipe/recipe.css';
 
 export default function Recipe() {
   return (
-    <div>
-      <h1>Recipe</h1>
+    <div className='page flex column'>
+      <div className="recipe-wrapper flex flex-grow-1 padding">
+        <div className='recipe flex-grow-1 border padding'>
+          <p>Main</p>
+        </div>
+      </div>
+      <div className="details-wrapper flex flex-grow-3 padding">
+        <div className='details flex column flex-grow-1'>
+          <div className='ingredients flex-grow-1 border padding'>
+            <p>Ingredients</p>
+          </div>
+          <div className='preparations flex-grow-1 border padding'>
+            <p>Preparations</p>
+          </div>
+          <div className='instructions flex-grow-1 border padding'>
+            <p>Instructions</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
