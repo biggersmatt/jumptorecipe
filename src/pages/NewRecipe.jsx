483cd49dff1f91da9bb5028e82fdff0a02e5d1f7
Matt Biggers <matt.biggers@workday.com>
2022-10-12 20:55:17 -0700

    Creates NewRecipe page and adds form onSubmit

--- src/pages/NewRecipe.jsx
+++ src/pages/NewRecipe.jsx
@@ -1,9 +1,134 @@
+// Dependencies
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+// CSS
 import '../components/NewRecipe/newrecipe.css';
 
 export default function NewRecipe() {
+  const { handleSubmit, register, formState: { errors } } = useForm();
+
+  const [recipe, setRecipe] = useState({
+    name: '',
+    description: '',
+    categories: [],
+    time: '',
+    ingredients: [],
+    preparations: [],
+    instructions: []
+  });
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
-    <div>
-      <h1>New Recipe</h1>
+    <div className='lightblue page flex column justify-center align-center'>
+      {/* Header Wrapper */}
+      <div className="flex justify-center width-100 padding-updown">
+        {/* <Header /> */}
+        <div className="plum header flex column align-center border radius width-fit shadow">
+          <h1 className="padding">New Recipe</h1>
+          <p className='padding'>Create a new recipe to for yourself and others</p>
+        </div>
+      </div>
+      {/* Recipe Wrapper */}
+      <div className='flex padding'>
+        {/* Recipe */}
+        <div className='coral recipe flex column align-center border radius padding shadow'>
+          <div className='flex column justify-center align-center padding'>
+            <h1 className='font-8vw'>Tofu Scramble</h1>
+          </div>
+          <div className='flex justify-center align-center padding'>
+            {/* <p className='width-90 font-4vw'>A high protein alternative to scrambled eggs! With the right spice, this scramble tastes better than traditional egg scrambles.</p> */}
+            <form onSubmit={handleSubmit(onSubmit)} className="flex column">
+              <input type="text" placeholder='Name' {...register("name")}/>
+              <textarea type="text" placeholder='Description' {...register("description")}/>
+              <input type="text" placeholder='Time to Cook' {...register("time")}/>
+              <select {...register("category", { required: true })}>
+                <option value="">Select...</option>
+                <option value="A">Option A</option>
+                <option value="B">Option B</option>
+              </select>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      {/* Details Wrapper*/}
+      <div className='flex justify-center flex-grow-3 width-100'>
+        {/* Details */}
+        <div className='details flex flex-grow-1'>
+          {/* Ingredients Wrapper*/}
+          <div className='ingredients-wrapper flex column'>
+            {/* Ingredients Container*/}
+            <div className="flex justify-center padding">
+              {/* Ingredients */}
+              <div className='plum detail flex column align-center flex-grow-1 border radius shadow'>
+                <div className="white flex justify-center width-100 border-bottom padding-updown">
+                  <h2 className='font-4vw'>Ingredients</h2>
+                </div>
+                <div className='white flex width-80 border radius margin-updown padding'>
+                  <p className='font-3vw padding'>16oz</p>
+                  <p className='font-3vw padding'>Firm Tofu</p>
+                </div>
+                <div className='white flex width-80 border radius margin-updown padding'>
+                  <p className='font-3vw padding'>1Tbsp</p>
+                  <p className='font-3vw padding'>Olive Oil</p>
+                </div>
+                <div className='white flex width-80 border radius margin-updown padding'>
+                  <p className='font-3vw padding'>2Tbsp</p>
+                  <p className='font-3vw padding'>Nutrional Yeast</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Preparations Wrapper*/}
+          <div className='preparations-wrapper flex column'>
+            {/* Preparations Container */}
+            <div className="flex justify-center padding">
+              {/* Preparations */}
+              <div className='aqua detail flex column align-center flex-grow-1 border radius shadow'>
+                <div className="white flex justify-center width-100 border-bottom padding-updown">
+                  <h2 className='font-4vw'>Preparations</h2>
+                </div>
+                <div className='white flex width-80 border radius margin-updown padding shadow'>
+                  <p className='font-3vw padding'>1:</p>
+                  <p className='font-3vw padding'>Press and drain tofu to remove moisture</p>
+                </div>
+                <div className='white flex width-80 border radius margin-updown padding shadow'>
+                  <p className='font-3vw padding'>2:</p>
+                  <p className='font-3vw padding'>Combine all spices in a separate dish and set aside</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Instructions Wrapper*/}
+          <div className='instructions-wrapper flex column'>
+            {/* Instructions Container */}
+            <div className="flex justify-center padding">
+            {/* Instructions */}
+            <div className='gold detail flex column align-center flex-grow-1 border radius shadow'>
+                <div className="white flex justify-center width-100 border-bottom padding-updown">
+                  <h2 className='font-4vw'>Instructions</h2>
+                </div>
+                <div className='white flex width-80 border radius margin-updown padding shadow'>
+                  <p className='font-3vw padding'>1:</p>
+                  <p className='font-3vw padding'>Add olive oil to a 10in pan on medium heat</p>
+                </div>
+                <div className='white flex width-80 border radius margin-updown padding shadow'>
+                  <p className='font-3vw padding'>2:</p>
+                  <p className='font-3vw padding'>Once the pan is hot, add tofu to the pan and cook for 5 minutes turning frequently</p>
+                </div>
+                <div className='white flex width-80 border radius margin-updown padding shadow'>
+                  <p className='font-3vw padding'>3:</p>
+                  <p className='font-3vw padding'>Add combined spices from preparations to the tofu and stir. Cook for 5 minutes</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
